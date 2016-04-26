e23d37060b47c9c694fcd4b22e75161e549f379e
Danielle Walker <danielle@bliss.net.au>
2016-04-27 00:44:04 +1000

    Increase Menu Font Size

--- src/MenuController.cs
+++ src/MenuController.cs
@@ -51,8 +51,8 @@ static class MenuController
 	private const int MENU_TOP = 575;
 	private const int MENU_LEFT = 30;
 	private const int MENU_GAP = 0;
-	private const int BUTTON_WIDTH = 75;
-	private const int BUTTON_HEIGHT = 15;
+	private const int BUTTON_WIDTH = 115;
+	private const int BUTTON_HEIGHT = 24;
 	private const int BUTTON_SEP = BUTTON_WIDTH + MENU_GAP;
 
 	private const int TEXT_OFFSET = 0;
